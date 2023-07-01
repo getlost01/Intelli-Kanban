fa8dbb2c010d1d05f0ca61cb694ed4a848cbf13c
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-07-01 16:56:37 +0530

    add more components for kanaban loading snackbar menu popover, and theme settings

---
+++ assets/illustrations/index.ts
@@ -0,0 +1,2 @@
+export { default as PageNotFoundIllustration } from './PageNotFoundIllustration';
+
---
+++ components/lightbox/index.ts
@@ -0,0 +1,4 @@
+export * from './types';
+export * from 'yet-another-react-lightbox';
+
+export { default } from './Lightbox';
